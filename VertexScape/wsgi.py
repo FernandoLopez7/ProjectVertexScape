1c97650297e35c0a63436138a9f0ac4b25cf1d3a
FernandoLopez7 <fernando.lopez.achig@udla.edu.ec>
2024-06-09 21:22:39 -0500

    Update

--- VertexScape/wsgi.py
+++ VertexScape/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VertexScape.settings')
 
 application = get_wsgi_application()
+
+if not settings.DEBUG:
+    from whitenoise import WhiteNoise
+    application = WhiteNoise(application, root=settings.STATIC_ROOT)
